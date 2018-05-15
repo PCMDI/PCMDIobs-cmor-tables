5fb0165594d2764bdf22e59a3a14f431531e0db6
Yuying Zhang <zhang24@llnl.gov>
2018-05-15 14:45:51 -0700

    add 2D code for TRMM to ./test

--- test/runCmor_TRMM.py
+++ test/runCmor_TRMM.py
@@ -6,10 +6,10 @@ import numpy as np
 
 #%% User provided input
 cmorTable = 'Tables/PMPObs_Amon.json' ; # Aday,Amon,Lmon,Omon,SImon,fx,monNobs,monStderr - Load target table, axis info (coordinates, grid*) and CVs
-inputJson = 'GPCP2.3-input.json' ; # Update contents of this file to set your global_attributes
+inputJson = 'TRMM-input.json' ; # Update contents of this file to set your global_attributes
 inputFilePathbgn = '/clim_obs/obs/atm/mo/'
-inputFilePathend = '/GPCP/'
-inputFileName = ['pr_GPCP_197901-200909.nc']
+inputFilePathend = '/TRMM/'
+inputFileName = ['pr_TRMM-L3_v7A_200001-200912.nc']
 inputVarName = ['pr']
 outputVarName = ['pr']
 outputUnits = ['kg m-2 s-1']
