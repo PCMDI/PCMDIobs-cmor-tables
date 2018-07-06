5a43b400447c39b4a03cd26e2b76e796f6dadfc3
Peter Gleckler <gleckler1@llnl.gov>
2018-07-06 15:29:39 -0700

    updated DRS added inputs

--- src/writeJson.py
+++ src/writeJson.py
@@ -219,6 +219,31 @@ source_id['source_id'][key]['region'] = ['global']
 source_id['source_id'][key]['source_variables'] = ['pr']
 source_id['source_id'][key]['source_version_number'] = '2.3'
 
+key = 'ERA-INT'
+source_id['source_id'][key] = {}
+source_id['source_id'][key]['source_description'] = 'ECMWF-ERAINT 1.0 (2010): ECMWF - ERAINT (European ReAnalysis 1989-2010)'
+source_id['source_id'][key]['institution_id'] = 'ECMWF'
+source_id['source_id'][key]['release_year'] = '2010'
+source_id['source_id'][key]['source_id'] = key
+source_id['source_id'][key]['source_label'] = 'ECMWF-ERAINT'
+source_id['source_id'][key]['source_name'] = 'ECMWF-ERAINT'
+source_id['source_id'][key]['source_type'] = 'reanalysis'
+source_id['source_id'][key]['region'] = ['global']
+source_id['source_id'][key]['source_variables'] = ['ta','ua','va','zg']
+source_id['source_id'][key]['source_version_number'] = 'N/A'
+
+key = 'JRA25'
+source_id['source_id'][key] = {}
+source_id['source_id'][key]['source_description'] = 'JRA25 1.0 (2002): JRA25 (Japanese ReAnalysis 1957-2002)'
+source_id['source_id'][key]['institution_id'] = 'MRI'
+source_id['source_id'][key]['release_year'] = '2002'
+source_id['source_id'][key]['source_id'] = key
+source_id['source_id'][key]['source_label'] = 'MRI-JRA25'
+source_id['source_id'][key]['source_name'] = 'MRI-JRA25'
+source_id['source_id'][key]['source_type'] = 'reanalysis'
+source_id['source_id'][key]['region'] = ['global']
+source_id['source_id'][key]['source_variables'] = ['ta','ua','va','zg']
+source_id['source_id'][key]['source_version_number'] = 'N/A'
 
 '''
 List from https://goo.gl/GySZ56 to be updated
