8e1e5a4e5875e2035712ee4421d6c03785b7d8a7
Peter Gleckler <gleckler1@llnl.gov>
2018-08-14 16:39:10 -0700

    added code to modify input entries globally

---
+++ inputs/modify_all_input_jsons.py
@@ -0,0 +1,39 @@
+import sys, os, json
+import glob
+from shutil import copyfile
+import time
+
+### 
+# THIS CODE IS USED TO CHANGE A KEY(S) ACROSS ALL INPUT JSONS
+
+lst = glob.glob('./*.json')
+
+for l in lst:
+## MV all FILE.json to FILE_cp.json 
+   cpf = l.replace('.json','_cp.json')
+   os.rename(l,cpf)
+   time.sleep(0.1)
+
+# LOAD DICTIONARY CONTENTS 
+   f =  open(cpf,'r')
+   d = json.load(f)
+   f.close()
+
+###########
+   d['outpath'] = '/p/user_pub/pmp/pmp_results/tree_v0.3/pmp_v1.1.2/data/'
+
+###########
+### SAVE CHANGED VALUES
+   time.sleep(0.1)
+   g =  open(l,'w+')
+   print d.keys()
+#  json.dumps(d,g)
+   json.dump(d,g,ensure_ascii=True,sort_keys=True,indent=4,separators=(',',':'),encoding="utf-8")
+   g.close()
+
+
+    
+
+
+
+  
