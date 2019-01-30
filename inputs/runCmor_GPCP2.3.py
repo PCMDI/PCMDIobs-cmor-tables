d27137e11d760b0b006878c7fd890a19e07dc768
Peter Gleckler <gleckler1@crunchy.llnl.gov>
2019-01-30 09:29:20 -0800

    updated path

--- inputs/runCmor_GPCP2.3.py
+++ inputs/runCmor_GPCP2.3.py
@@ -5,7 +5,7 @@ import numpy as np
 #import pdb ; # Debug statement - import if enabling below
 
 #%% User provided input
-cmorTable = 'Tables/PMPObs_Amon.json' ; # Aday,Amon,Lmon,Omon,SImon,fx,monNobs,monStderr - Load target table, axis info (coordinates, grid*) and CVs
+cmorTable = '../Tables/PMPObs_Amon.json' ; # Aday,Amon,Lmon,Omon,SImon,fx,monNobs,monStderr - Load target table, axis info (coordinates, grid*) and CVs
 inputJson = 'GPCP2.3-input.json' ; # Update contents of this file to set your global_attributes
 inputFilePathbgn = '/clim_obs/obs/atm/mo/'
 inputFilePathend = '/GPCP/'
--- inputs/test_run_all.sh
+++ inputs/test_run_all.sh
@@ -1,2 +1,12 @@
-runCmor_CERES4.0_2D.py
-runCmor_ERA-INT_2.py
+python runCmor_CERES4.0_2D.py
+python runCmor_ERA-INT_2.py
+python runCmor_CERES4.0_SURFACE_2D.py
+python runCmor_ERA40_2D.py
+python runCmor_ERA40_3D.py
+python runCmor_ERAINT_2D.py
+python runCmor_ERAINT_3D.py
+python runCmor_GPCP2.3.py
+python runCmor_JRA25_2D.py
+python runCmor_RSS_2D.py
+python runCmor_JRA25_3D.py
+python runCmor_TRMM.py
