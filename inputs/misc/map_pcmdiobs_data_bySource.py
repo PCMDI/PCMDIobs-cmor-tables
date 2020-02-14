3c929b74b8779b0d5b82ab90ab3907897586904a
Peter Gleckler <gleckler1@gates.llnl.gov>
2020-02-13 17:21:11 -0800

    working on bySource

--- inputs/misc/map_pcmdiobs_data_bySource.py
+++ inputs/misc/map_pcmdiobs_data_bySource.py
@@ -7,13 +7,17 @@ base_dir = '/p/user_pub/PCMDIobs/'
 data_ver = 'PCMDIobs2.0-beta'
 root = base_dir + '/' + data_ver + '/'
 
+lstx = ['TRMM-3B43v-7', 'ERA-5']
+
+
 try:
  new_root = root +'bySource'
  os.mkdir(new_root)
 except:
  pass
 
-lst = glob.glob(root + '*/*/*/*/*/*/*.nc')
+lst = glob.glob(root + '*/mon/*/*/*/latest/*.nc')
+lstx = glob.glob(root + '*/*/*/*/1x1/latest/*.xml')
 products = []
 for l in lst: 
   p = l.split('/')[9]
@@ -30,11 +34,21 @@ for p in products:
  except:
   pass
 
- pp= root + '*/*/*/' + p + '/*/*/*.nc'
+ pp= root + '*/*/*/' + p + '/*/latest/*.nc'
  lstp = glob.glob(pp)
  print(p,' ', len(lstp))
 
  for l in lstp:
    fn = l.split('/')[12]
+   if 'ERA-5' not in fn and 'TRMM-3B43v-7' not in fn:
+    print('----- ', fn) 
+    print(l,new_root + '/' + p + '/' + fn)
+    os.symlink(l,new_root + '/' + p + '/' + fn)
 
-   os.symlink(l,new_root + '/' + p + '/' + fn)
+for l in lstx:
+    fn = l.split('/')[12]
+    p = l.split('/')[9]
+    print('+++++ ', ) 
+    print(new_root + '/' + p + '/' + fn)
+    os.symlink(l,new_root + '/' + p + '/' + fn)
+
